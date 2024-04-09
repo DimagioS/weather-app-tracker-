0c6ef2e6eceaf2c46ced34249d7afa14a0972d84
Dmitry Sushkov <sushkovd98@mail.ru>
2024-04-09 10:09:57 +0200

    Add FSD base project structure

---
+++ src/entities/Citiy/ui/CityCard/CityCard.jsx
@@ -0,0 +1,9 @@
+import styles from './CityCard.module.scss';
+
+const CityCard = () => {
+  return (
+    <div className={styles.CityCard}>CityCard</div>
+  );
+}
+
+export default CityCard;
---
+++ src/entities/Citiy/ui/CityList/CityList.jsx
@@ -0,0 +1,9 @@
+import styles from './CityList.module.scss';
+
+const CityList = () => {
+  return (
+    <div className={styles.CityList}>CityList</div>
+  );
+}
+
+export default CityList;
